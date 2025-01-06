146b7029d28400be2f93c44012db32b5c0d8484e
JaredAtti <jaredatti@gmail.com>
2025-01-06 12:43:41 -0500

    add printed report of book

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
 def main():
     book_path = "books/frankenstein.txt"
 
-    text = get_book_text(book_path)
-    num_words = get_num_words(text)
-    char_dict = get_chars(text)
-
-    # print(f"{num_words} words found in the document")
-    print(char_dict)
-
-
+    print_report(book_path)
 
 def get_book_text(path):
     with open(path) as f:
@@ -24,5 +17,33 @@ def get_chars(text):
         c = i.lower()
         dict[c] = dict.get(c, 0) + 1
     return dict
+
+def print_report(path):
+    text = get_book_text(path)
+    num_words = get_num_words(text)
+    char_dict = get_chars(text)
+
+    print(f"--- Begin report of {path} ---")
+    print(f"{num_words} words found in the document\n")
+
+    listed_chars = dictToList(char_dict)
+    listed_chars.sort(reverse=True, key=sort_on)
+    
+    for i in listed_chars:
+        print(f"The '{i['char']}' character was found {i['count']} times")
+
+    print(f"--- End Report ---")
+
+
+def dictToList(dict):
+    converted = []
+    for i in dict:
+        if i.isalpha():
+            converted.append({"char": i, "count": dict[i]})
+    
+    return converted
+
+def sort_on(dict):
+    return dict["count"]
 
 main()
