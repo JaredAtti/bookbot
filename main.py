3e2026ee9a4daed66fd17f5f8cf152c83581675b
JaredAtti <jaredatti@gmail.com>
2025-01-06 12:23:22 -0500

    add getting chars dictionary

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 def main():
     book_path = "books/frankenstein.txt"
+
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    print(f"{num_words} words found in the document")
+    char_dict = get_chars(text)
+
+    # print(f"{num_words} words found in the document")
+    print(char_dict)
+
 
 
 def get_book_text(path):
@@ -12,5 +17,12 @@ def get_book_text(path):
 def get_num_words(text):
     words = text.split()
     return len(words)
+
+def get_chars(text):
+    dict = {}
+    for i in text:
+        c = i.lower()
+        dict[c] = dict.get(c, 0) + 1
+    return dict
 
 main()
